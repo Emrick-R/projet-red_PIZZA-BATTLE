9c525ef518ebff3c3d785835c215f69e5cec7ca4
Emrick <emrick.rivet@ynov.com>
2025-09-12 14:21:55 +0200

    "Modif TakePoisonPot"

--- src/items/items.go
+++ src/items/items.go
@@ -21,10 +21,14 @@ func TakePot(c *structures.Character) {
 		}
 	}
 	fmt.Println("Il n'y a pas de potions dans l'inventaire")
-
+	PoisonPot := structures.Object{"Potion de Poison", 1}
 	func ThrowPoisonPot(c *structures.Character) {
 		for i := 0; i < len(c.Inventory); i++ {
-		if c.Inventory[i].Name == .Name {
+		if c.Inventory[i].Name == PoisonPot.Name {
+			
+			if c.Inventory[i].Quantity == 0 {
+				c.Inventory = append(c.Inventory[:i], c.Inventory[i+1:]...)
+			}
 
 
 
