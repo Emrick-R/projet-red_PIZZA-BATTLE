24fc15cc4a9b06f9494954e71f130ee6ae6732e9
Emrick <emrick.rivet@ynov.com>
2025-09-16 11:50:56 +0200

    "Début de fonction combat par tour"

--- src/combat/combat.go
+++ src/combat/combat.go
@@ -9,9 +9,16 @@ func EnemyPatern() {
 	*/
 }
 
-func Turn() {
+func TurnCombat1v1() {
 	/* 1v1 =
 	Enemy commence tour 1 et chaque tour+2 (T1,T3,T5,T7 etc.)
 	Joueur commence tour 2 et chaque Tour+2 (T2,T4,T6,T8,etc.)*/
-
+	Turn := 1
+	if Turn%2 == 0 { //Le tour du joueur, donc Tour 2
+		//Appel de la fonction tour du Joueur
+		Turn++
+	} else { //Le tour de l'IA, donc Tour 2
+		//Appel de la fonction du tour de l'IA
+		Turn++
+	}
 }
