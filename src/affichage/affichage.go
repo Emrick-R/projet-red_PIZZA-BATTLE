d8efb55d17e2aee38b7a49eccef2c7beb70d5cc9
Harld9 <harold.francois@ynov.com>
2025-09-16 12:15:00 +0200

    Changement affichage

--- src/affichage/affichage.go
+++ src/affichage/affichage.go
@@ -2,26 +2,60 @@ package affichage
 
 import "fmt"
 
+func separator() {
+	fmt.Println("======================================")
+}
+
 func AffichageMenuPrincipal() {
-	fmt.Println("======== Faites votre choix : ========")
+	separator()
+	fmt.Println("🍕 BIENVENUE DANS PIZZA BATTLE 🍕")
+	separator()
 	fmt.Println("1 - Commencer une nouvelle partie")
 	fmt.Println("2 - Quitter")
+	separator()
 }
 
 func AffichageMenuPersonnage() {
-	fmt.Println("\n======== Menu Personnage : ========")
+	separator()
+	fmt.Println("👤 Menu Personnage :")
+	separator()
 	fmt.Println("1 - Afficher le personnage")
 	fmt.Println("2 - Afficher l'inventaire")
 	fmt.Println("3 - Test de combat : Utiliser une potion de poison")
 	fmt.Println("4 - Marchand")
 	fmt.Println("5 - Forgeron")
 	fmt.Println("6 - RETOUR")
-
+	separator()
 }
 
 func AffichageMenuInventaire() {
-	fmt.Println("======== Menu Inventaire : ========")
+	separator()
+	fmt.Println("🎒 Inventaire :")
+	separator()
 	fmt.Println("1 - Utiliser une potion")
 	fmt.Println("2 - Equiper un équipement")
 	fmt.Println("3 - RETOUR")
+	separator()
+}
+
+func CharacterTurn() {
+	separator()
+	fmt.Println("⚔️  Combat :")
+	separator()
+	fmt.Println("1 - Attaquer")
+	fmt.Println("2 - Inventaire")
+	fmt.Println("3 - RETOUR")
+	separator()
+}
+
+// Pour afficher les attaques, passe la liste des skills en paramètre
+func Attaques(skills []string) {
+	separator()
+	fmt.Println("💥 Attaques :")
+	for i, skill := range skills {
+		fmt.Printf("%d - %s\n", i+1, skill)
+	}
+	fmt.Println("0 - RETOUR")
+	separator()
+
 }
